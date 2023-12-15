d74c1192656816bf232f8e2c12e9debd151cf351
Sokhatai <auguin.corentin@orange.fr>
2023-12-15 03:34:09 +0100

    Part 2 terminé

--- script.js
+++ script.js
@@ -5,27 +5,31 @@ function exo() {
     let maxGreens = 13;
     let maxReds = 12;
     let result = 0;
+    let power = 0;
     games.shift();
     for(let game of games){ 
         let gameId = game.match(/\d+/);                                          /*on récupère l'id de la partie*/
         let blues = game.match(/\d+ blue/g);
         let bluePossible = testNumber(blues, maxBlues);
+        let maxBluesNeeded = testMax(blues);
         let greens = game.match(/\d+ green/g);
         let greenPossible = testNumber(greens, maxGreens);
+        let maxGreensNeeded = testMax(greens);
         let reds = game.match(/\d+ red/g);
-        console.log(reds);
-        let redPossible = testNumber(reds, maxReds);                              /*on va tester pour chaque game si c'est possible avec le maximum de cube demandé */
+        let redPossible = testNumber(reds, maxReds);
+        let maxRedsNeeded = testMax(reds);                              /*on va tester pour chaque game si c'est possible avec le maximum de cube demandé et récupérer le maximun demandé*/
         if (bluePossible && greenPossible && redPossible){
             result = result + parseInt(gameId);            
         }
+        power = power + (maxBluesNeeded*maxGreensNeeded*maxRedsNeeded);
     }
     let showResult = document.getElementById("result");
-    showResult.innerHTML = "<p>Le résultat obtenu est " + result + "</p>";  /* On affiche le résultat obtenu */
+    showResult.innerHTML = "<p>Le résultat de ma première partie de l'exo est " + result + "</br> et " + power + " pour la deuxième partie </p>";  /* On affiche le résultat obtenu */
 }
 
 function testNumber(cubes, max){
     if (cubes == null){
-        return 1;
+        return 0;
     }
     for (setOfCubes of cubes){
             numberOfCubes = parseInt(setOfCubes.match(/\d+/));
@@ -37,7 +41,21 @@ function testNumber(cubes, max){
 
 }
 
-
+function testMax(cubes){
+    let maxOfCubes = 0;
+    if (cubes == null){
+        return 0;
+    }
+    for (setOfCubes of cubes){
+        console.log(setOfCubes);
+        numberOfCubes = parseInt(setOfCubes.match(/\d+/));
+        if (numberOfCubes > maxOfCubes){
+            maxOfCubes = numberOfCubes;
+        }
+    }
+    
+    return maxOfCubes;
+}
 
 
 const selection = document.getElementById("input");
